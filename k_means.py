2d36c3be791966a1aa611ef3d25c986633fa0f20
Galen Berger-Fletcher <bergerg@carleton.edu>
2018-02-20 17:56:00 -0600

    improved efficiency of build_clusters by storing centroids in a seperate list

--- k_means.py
+++ k_means.py
@@ -28,10 +28,12 @@ def euclidean_distance(p, q):
 def build_clusters(data):
     k = decide_k()
     clusters = []
+    centroids = []
 
     for cluster in range(0, k):
         initial_item = [data.pop(0)]
         clusters.append(initial_item)
+        centroids.append(centroid(clusters[cluster]))
 
     for item in data:
         distance = float(item[1])
@@ -41,12 +43,13 @@ def build_clusters(data):
         smallest_distance = 1000
 
         for cluster in clusters:
-            centroid_distance = euclidean_distance(centroid(cluster), (distance, speed))
+            centroid_distance = euclidean_distance(centroids[clusters.index(cluster)], (distance, speed))
             if centroid_distance < smallest_distance:
                 smallest_distance = centroid_distance
                 closest_cluster = clusters.index(cluster)
 
         clusters[closest_cluster].append(item)
+        centroids[closest_cluster] = centroid(clusters[closest_cluster])
 
     done = False
     loops = 0
@@ -59,18 +62,21 @@ def build_clusters(data):
                 distance = float(item[1])
                 speed = float(item[2])
 
-                cur_centroid_distance = euclidean_distance(centroid(cluster), (distance, speed))
+                cur_centroid_distance = euclidean_distance(centroids[clusters.index(cluster)], (distance, speed))
                 cur_cluster = cluster
 
                 for comp_cluster in clusters:
-                    centroid_distance = euclidean_distance(centroid(comp_cluster), (distance, speed))
+                    centroid_distance = euclidean_distance(centroids[clusters.index(comp_cluster)], (distance, speed))
                     if centroid_distance < cur_centroid_distance:
                         cur_centroid_distance = centroid_distance
                         cur_cluster = comp_cluster
 
                 if cur_cluster != cluster:
+                    updated_clusters = [clusters.index(cluster), clusters.index(cur_cluster)]
                     cluster.remove(item)
                     cur_cluster.append(item)
+                    for updated_cluster in updated_clusters:
+                        centroids[updated_cluster] = centroid(clusters[updated_cluster])
                     done = False
 
         if loops > 20:
